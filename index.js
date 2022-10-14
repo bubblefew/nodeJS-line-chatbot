e9164f4cac7dd75cf84802dcfd3d3e3fcddfcc40
Jilasak ICT <bubble.few@gmail.com>
2022-10-14 16:25:37 +0700

    first commit

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ app.post('/callback', line.middleware(config), async (req, res) => {
             const events = req.body.events
             let sysId = 0;
             if (result == "pass") {
-                const data = await query(`SELECT * FROM cr_control `);
+                const data = await query(`SELECT CRC_PYNO FROM cr_control `);
                 if (events) {
                     let custRes = await loadCustomer(req.body.events[0].source.userId);
                     // let { userId, displayName, pictureUrl } = JSON.parse(custRes);
-                    console.log(events)
-                    client.replyMessage(events[0].replyToken, { type: 'text', text: "few" });
+                    console.log(data[0].CRC_PYNO)
+                    client.replyMessage(events[0].replyToken, st);
                 } else {
                     console.log("Failed")
                 }
@@ -79,6 +79,43 @@ app.post('/callback', line.middleware(config), async (req, res) => {
 
 //     }
 // }
+
+
+const st = {
+    "type": "template",
+    "altText": "This is a buttons template",
+    "template": {
+        "type": "buttons",
+        "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+        "imageAspectRatio": "rectangle",
+        "imageSize": "cover",
+        "imageBackgroundColor": "#FFFFFF",
+        "title": "Menu",
+        "text": "Please select",
+        "defaultAction": {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+        },
+        "actions": [
+            {
+                "type": "postback",
+                "label": "Buy",
+                "data": "action=buy&itemid=123"
+            },
+            {
+                "type": "postback",
+                "label": "Add to cart",
+                "data": "action=add&itemid=123"
+            },
+            {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+            }
+        ]
+    }
+}
 
 function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
