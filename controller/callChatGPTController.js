1e7b7721ec1d8638fd0a2f5578e82e33937f4781
bubblefew <bubble.few@gmail.com>
2023-04-06 08:55:19 +0700

    few

--- controller/callChatGPTController.js
+++ controller/callChatGPTController.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 
 module.exports.getChatbotResponse = async (req, res, next) => {
+  console.log("GPT AAA");
   const API_ENDPOINT =
     "https://api.openai.com/v1/engines/davinci-codex/completions";
   var input = "Hello chat gpt. my name is few. ";
-  const API_KEY = "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"; // Replace with your OpenAI API key
+  const API_KEY = "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"; // Replace with your OpenAI API key
   const prompt = `The following is a conversation between a human and a chatbot. The human is trying to ${input}. The chatbot responds:`;
   const data = {
     prompt: prompt,
@@ -20,5 +21,5 @@ module.exports.getChatbotResponse = async (req, res, next) => {
   const response = await axios.post(API_ENDPOINT, data, { headers: headers });
   const chatbotResponse = response.data.choices[0].text.trim();
   console.log(chatbotResponse);
-  return chatbotResponse;
+  // return chatbotResponse;
 };
--- controller/unlockCreditController.js
+++ controller/unlockCreditController.js
@@ -1,21 +1,31 @@
-const { executeSQL } = require("../resource/callMysql");
 const { exec } = require("child_process");
+const { executeSQL } = require("../resource/callMysql");
 
 module.exports.unlockCreditLimit = async (req, res, next) => {
   try {
-    const jarPath = "D:/is/OIS2120_AUTO/dist/OIS2120_AUTO.jar";
-
-    exec(`java -jar ${jarPath}`, (error, stdout, stderr) => {
+    // const jarPath = "D:/is/OIS2120_AUTO/dist/OIS2120_AUTO.jar";
+    const jarPath =
+      "D:/java_project/JavaApplication2/dist/JavaApplication2.jar";
+    let { reqno, lineId } = req.body;
+    exec(`java -jar ${jarPath}`, async (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
       }
+      let sql = `UPDATE is.requestheader
+      SET H_Status = '50' 
+      WHERE H_RequestNumber='${reqno}' `;
+
+      await executeSQL(sql);
+
       console.log(`stdout: ${stdout}`);
       console.error(`stderr: ${stderr}`);
       res
         .send({ message: "Unlock Credit on M3 Successfuly!", Status: "OK" })
         .status(200)
         .end();
+      let api = `http://localhost:3000/api/v1/chatbot/message`;
+      // lineId
     });
   } catch (error) {
     next(error);
